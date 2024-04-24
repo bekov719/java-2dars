a2332f2c143bfddadab1e77d06245a2950972aa4
bekov719 <odilbekhaitkulov@gmail.com>
2024-04-24 17:29:26 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const num1 = Math.floor(Math.random() * 100) + 1;
+const num2 = Math.floor(Math.random() * 100) + 1;
+const product = num1 * num2;
+const halfProduct = product / 2;
+alert("Случайные числа: " + num1 + " и " + num2);
+alert("Их произведение: " + product);
+alert("Половина произведения: " + halfProduct);
